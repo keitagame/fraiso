b3e5c315166eb43a28818f26d585059325f08684
keita <keitagame0319@icloud.com>
2025-08-18 09:12:25 +0000

    Update ownbuild.sh

--- ownbuild.sh
+++ ownbuild.sh
@@ -100,8 +100,8 @@ echo "Welcome to MyArch Live!" > "$AIROOTFS/root/README.txt"
 
 # ===== squashfs 作成 =====
 echo "[*] squashfs イメージ作成..."
-mkdir -p "$ISO_ROOT/arch"
-mksquashfs "$AIROOTFS" "$ISO_ROOT/arch/airootfs.sfs"  -comp xz -Xbcj x86
+mkdir -p "$ISO_ROOT/arch/$ARCH"
+mksquashfs "$AIROOTFS" "$ISO_ROOT/arch/$ARCH/airootfs.sfs"  -comp xz -Xbcj x86
 
 
 # ===== ブートローダー構築 (systemd-boot UEFI) =====
